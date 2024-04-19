e33027d668637742b14473dca5eda38367e1c212
Mattias Ekendahl <mattias@developedbyme.com>
2024-04-19 10:08:36 +0200

    Update: vite example

--- mongoaggregation/index.js
+++ mongoaggregation/index.js
@@ -21,6 +21,14 @@ app.get("/", async (request, response) => {
     }
 );
 
+app.get("/products", async (request, response) => {
+
+//METODO: connect to database
+    response.json([{"id": 1, "name": "Product 1"}, {"id": 2, "name": "Product 2"}]);
+
+    }
+);
+
 app.post("/create-order", async (request, response) => {
     
     let customer = await DatabaseConnection.getInstance().getOrCreateCustomer(request.body.email, request.body.name,  request.body.address);
---
+++ vite/src/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import ReactDOM from "react-dom/client";
+
+
+const App = () => {
+
+  return (
+    <div>New project</div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(<App />);
